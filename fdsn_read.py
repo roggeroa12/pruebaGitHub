2d2ae3a4afe327a452b6cdc89d3686af8af47b13
roger <62741545+roggeroa12@users.noreply.github.com>
2025-12-01 08:54:09 -0800

    se quita parte del codigo que grafica

--- fdsn_read.py
+++ fdsn_read.py
@@ -20,26 +20,3 @@ inv = client.get_stations(
 )
 
 print(inv)  # resumen
-# inv.plot()  # opcional: mapa/estaciones (abre ventana)
-
-# 2) Descargar formas de onda
-st = client.get_waveforms(
-    network="IU",
-    station="ANMO",
-    location="00",
-    channel="BHZ",
-    starttime=t0,
-    endtime=t1
-)
-
-print(st)
-st.detrend("linear")
-st.filter("bandpass", freqmin=0.5, freqmax=5.0)
-
-# 3) Guardar miniSEED
-st.write("waveform.mseed", format="MSEED")
-
-# 4) (Opcional) quitar respuesta y convertir a velocidad (m/s)
-st_corr = st.copy()
-st_corr.remove_response(inventory=inv, output="VEL", pre_filt=(0.1, 0.2, 10, 12))
-st_corr.write("waveform_vel.mseed", format="MSEED")
